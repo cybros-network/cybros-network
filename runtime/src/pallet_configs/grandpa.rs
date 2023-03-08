e634d83c62d5c86817f693fe84694b9159418c05
jasl <jasl9187@hotmail.com>
2023-03-08 17:25:12 +0800

    wip

--- runtime/src/pallet_configs/grandpa.rs
+++ runtime/src/pallet_configs/grandpa.rs
@@ -1,16 +1,11 @@
 use crate::*;
-use frame_support::traits::KeyOwnerProofSystem;
-use pallet_grandpa::AuthorityId as GrandpaId;
-use sp_core::{crypto::KeyTypeId, ConstU32, ConstU64};
+use sp_core::{ConstU32, ConstU64};
 
 impl pallet_grandpa::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
-	type KeyOwnerProof = <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(KeyTypeId, GrandpaId)>>::Proof;
-	type KeyOwnerIdentification =
-		<Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(KeyTypeId, GrandpaId)>>::IdentificationTuple;
-	type KeyOwnerProofSystem = ();
-	type HandleEquivocation = ();
 	type WeightInfo = ();
 	type MaxAuthorities = ConstU32<32>;
 	type MaxSetIdSessionEntries = ConstU64<0>;
+	type KeyOwnerProof = sp_core::Void;
+	type EquivocationReportSystem = ();
 }
