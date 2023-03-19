1ce616c98ab50966a9c9558d88286c702d070bcd
jasl <jasl9187@hotmail.com>
2023-03-19 08:26:55 +0800

    wip

--- protocol_impl/main.ts
+++ protocol_impl/main.ts
@@ -364,7 +364,7 @@ async function handleTask() {
 
     const result = code === 0 ? "Success" : "Failed";
     const taskResult = api.createType("TaskResult", result);
-    const taskOutput = api.createType("String", hexToString(parsedOut)) // TODO:
+    const taskOutput = api.createType("String", hexToString(parsedOut)) // TODO: FIXME, polkadot.js double encode hex
 
     logger.info(`Sending "pool_computing.submitTaskResult()`);
     const txPromise = api.tx.poolComputing.submitTaskResult(window.subscribePool, task.id, taskResult, taskOutput, null, null);
