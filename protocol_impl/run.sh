5f7eb76f81629a84c2e46aa7a0acfda15de329dd
jasl <jasl9187@hotmail.com>
2023-05-25 19:16:17 +0800

    wip

--- protocol_impl/run.sh
+++ protocol_impl/run.sh
@@ -3,11 +3,11 @@ set -e
 
 DENO_PATH=$(which deno)
 APP_PATH=$(dirname "$(readlink -f "$0")")
-EXECUTOR_PATH="$APP_PATH/examples/simple_echo/"
+EXECUTOR_PATH="${EXECUTOR_PATH:-"$APP_PATH/examples/simple_echo"}"
 
 deno run \
   --allow-run="$DENO_PATH" \
   --allow-net \
-  --allow-write="$APP_PATH/data/,$APP_PATH/tmp/,$APP_PATH/log/" \
-  --allow-read="$DENO_PATH,$PWD,$APP_PATH/data/,$APP_PATH/tmp/,$APP_PATH/log/,$EXECUTOR_PATH" \
+  --allow-write="$APP_PATH/data,$APP_PATH/tmp,$APP_PATH/log" \
+  --allow-read="$DENO_PATH,$PWD,$APP_PATH/data,$APP_PATH/tmp,$APP_PATH/log,$EXECUTOR_PATH" \
   "$APP_PATH"/main.ts --task-executor-path "$EXECUTOR_PATH" "$@"
