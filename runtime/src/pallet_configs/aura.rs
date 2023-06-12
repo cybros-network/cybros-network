ebbb2565b3eb7fb172957a9d745330e8ad5c81a2
jasl <jasl9187@hotmail.com>
2023-06-13 06:55:29 +0800

    wip

--- runtime/src/pallet_configs/aura.rs
+++ runtime/src/pallet_configs/aura.rs
@@ -1,9 +1,10 @@
 use crate::*;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use frame_support::traits::ConstU32;
+use frame_support::traits::{ConstBool, ConstU32};
 
 impl pallet_aura::Config for Runtime {
 	type AuthorityId = AuraId;
 	type MaxAuthorities = ConstU32<32>;
 	type DisabledValidators = ();
+	type AllowMultipleBlocksPerSlot = ConstBool<false>;
 }
