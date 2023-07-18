79480739566996bca33efb336b99f2db8fa02824
jasl <jasl9187@hotmail.com>
2023-07-19 03:42:57 +0800

    wip

--- runtimes/primal/src/pallet_configs/grandpa.rs
+++ runtimes/primal/src/pallet_configs/grandpa.rs
@@ -23,6 +23,7 @@ impl pallet_grandpa::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = ();
 	type MaxAuthorities = ConstU32<32>;
+	type MaxNominators = ConstU32<0>;
 	type MaxSetIdSessionEntries = ConstU64<0>;
 	type KeyOwnerProof = sp_core::Void;
 	type EquivocationReportSystem = ();
