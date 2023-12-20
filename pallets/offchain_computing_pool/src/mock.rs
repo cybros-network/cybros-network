4e6bb95c52e73325eeebccb0e2904311be63f1a3
Jun Jiang <jasl9187@hotmail.com>
2023-12-20 11:25:40 +0800

    wip

--- pallets/offchain_computing_pool/src/mock.rs
+++ pallets/offchain_computing_pool/src/mock.rs
@@ -23,9 +23,9 @@ use frame_support::{
 	traits::{OnFinalize, OnInitialize},
 };
 use frame_system::EnsureSigned;
-use sp_core::{ConstBool, ConstU128, ConstU16, ConstU32, ConstU64, H256};
+use sp_core::{ConstBool, ConstU128, ConstU16, ConstU32, ConstU64};
 use sp_runtime::{
-	traits::{BlakeTwo256, IdentifyAccount, IdentityLookup, Verify},
+	traits::{IdentifyAccount, IdentityLookup, Verify},
 	BuildStorage, MultiSignature,
 };
 
@@ -58,28 +58,12 @@ frame_support::construct_runtime!(
 
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
 impl frame_system::Config for Test {
-	type RuntimeEvent = RuntimeEvent;
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type RuntimeOrigin = RuntimeOrigin;
-	type RuntimeCall = RuntimeCall;
-	type Nonce = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
 	type AccountId = AccountId;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Block = Block;
-	type BlockHashCount = ConstU64<250>;
-	type DbWeight = ();
-	type Version = ();
 	type PalletInfo = PalletInfo;
 	type AccountData = pallet_balances::AccountData<Balance>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
 	type SS58Prefix = ConstU16<42>;
-	type OnSetCode = ();
 	type MaxConsumers = ConstU32<16>;
 }
 
